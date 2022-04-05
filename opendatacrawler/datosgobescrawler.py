d84b0d384cb23747f021388b6ae85581131b57e3
Alberto Berenguer Pastor <alberto.berenguer@ua.es>
2022-04-05 14:02:49 +0200

    datos.gob.es Module(Fix)

--- opendatacrawler/datosgobescrawler.py
+++ opendatacrawler/datosgobescrawler.py
@@ -1,6 +1,5 @@
-from pickle import TRUE
 import requests
-import utils
+import traceback
 from setup_logger import logger
 from opendatacrawlerInterface import OpenDataCrawlerInterface as interface
 
@@ -22,9 +21,9 @@ class datosGobEsCrawler(interface):
                 packages = response.json()['result']['items']
                 if len(packages) > 0:
                     skip += 1
-                    print(skip)
                     for p in packages:
                         ids.append(p['_about'].split("/")[-1])
+                    break
                 else:
                     fin = True
             else:
@@ -32,8 +31,17 @@ class datosGobEsCrawler(interface):
 
         return ids
 
+    def add_source(self, meta):
+        aux = dict()
+
+        aux['name'] = meta['title'][0]['_value']
+        aux['downloadUrl'] = meta.get('accessURL', None)
+        aux['mediaType'] = meta['format']['value']
+
+        return aux
+
     def get_package(self, id):
-    # Obtain a package with all their metadata
+        # Obtain a package with all their metadata
         try:
             url = "https://datos.gob.es/apidata/catalog/dataset/" + id
             response = requests.get(url)
@@ -43,24 +51,33 @@ class datosGobEsCrawler(interface):
 
                 metadata = dict()
 
-                metadata['identifier'] = meta['identifier']
+                metadata['identifier'] = id
                 metadata['title'] = meta['title'][0]['_value']
                 metadata['description'] = meta['description'][0]['_value']
-                metadata['theme'] = meta['theme'].split("/")[-1]
 
+                if not isinstance(meta['theme'], list):
+                    metadata['theme'] = meta.get('theme', None).split('/')[-1]
+                else:
+                    metadata['theme'] = [m.split('/')[-1] for m in meta['theme']]
+                
                 resource_list = []
+
+                if not isinstance(meta['distribution'], list):
+                    meta['distribution'] = [meta['distribution']]
+
                 for res in meta['distribution']:
-                    if (self.data_types is None or
-                    res['format']['value'].lower() in self.data_types):
-                        aux = dict()
-                        aux['name'] = res['title']['_value']
-                        aux['downloadUrl'] = res.get('accessURL', None)
-                        aux['mediaType'] = res['format']['value']
+                    if self.data_types:
+                        for t in self.data_types:
+                            if t in res['format']['value'].lower():
+                                aux = self.add_source(res)
+                                resource_list.append(aux)
+                    else:
+                        aux = self.add_source(res)
                         resource_list.append(aux)
 
                 metadata['resources'] = resource_list
-                metadata['modified'] = meta['modified']
-                metadata['license'] = meta['license']
+                metadata['modified'] = meta.get('modified', None)
+                metadata['license'] = meta.get('license', None)
                 metadata['source'] = self.domain
 
                 return metadata
@@ -68,5 +85,6 @@ class datosGobEsCrawler(interface):
                 return None
 
         except Exception as e:
+            print(traceback.format_exc())
             logger.error(e)
             return None
