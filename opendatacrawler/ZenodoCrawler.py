f68c4fb1c6aad331e0ba6fe597c2d142ef2010e5
Paula Gonzalez Martinez <pgm136@alu.ua.es>
2022-11-29 12:56:17 +0100

    Obtener datos solo tipo CSV

--- opendatacrawler/ZenodoCrawler.py
+++ opendatacrawler/ZenodoCrawler.py
@@ -22,8 +22,18 @@ class ZenodoCrawler(interface):
         skip = 1
         ids = []
         fin = False
+        
+        # records_api_url = 'https://zenodo.org/api/records'
+        # search_query = 'type:dataset AND file_type:(csv OR zip)'
+        
         while not fin:
-            response = requests.get('https://zenodo.org/api/records/?type=dataset&size=200&page='+str(skip)+'&access_token='+str(self.token))
+            # response = requests.get('https://zenodo.org/api/records/?type=dataset&file_type=csv&size=200&page='+str(skip)+'&access_token='+str(self.token))
+            response = requests.get('https://zenodo.org/api/records/',
+                        params={'type': 'dataset',
+                                'file_type': 'csv',
+                                'size': 200,
+                                'page': skip,
+                                'access_token': self.token})
             if response.status_code == 200:
                 packages = response.json()['hits']['hits']
                 if len(packages) > 0:
@@ -49,53 +59,34 @@ class ZenodoCrawler(interface):
 
                 metadata = dict()
                 
+                metadata['identifier'] = id
+                
+                meta = meta_json.get('metadata', None)
+                
+                if meta is not None:
+                    metadata['title'] = meta.get('title', None)
+                    metadata['description'] = meta.get('description', None)
+                    if meta.get('keywords', None) is not None:
+                        metadata['theme'] = utils.extract_keywords(meta.get('keywords', None)[0])
+                
+                resource_list = []
+
+                aux = dict()
+
                 if meta_json.get('files', None) is not None:
                     url = meta_json.get('files', None)[0]
-                    mediaType = url.get('type', None)
+                    aux['downloadUrl'] = url.get('links', None).get('self', None)
+                    aux['mediaType'] = url.get('type', None)
+
+                resource_list.append(aux)
+
+                metadata['resources'] = resource_list
+                metadata['modified'] = meta.get('publication_date', None)
+                #metadata['license'] = requests.get('https://zenodo.org/api/licenses/')
+                metadata['license'] = None
+                metadata['source'] = self.domain
                 
-                    if mediaType == "csv" or mediaType == "zip" or mediaType == "excel":
-                        
-                        metadata['identifier'] = id
-                        
-                        meta = meta_json.get('metadata', None)
-                        
-                        if meta is not None:
-                            metadata['title'] = meta.get('title', None)
-                            metadata['description'] = meta.get('description', None)
-                            if meta.get('keywords', None) is not None:
-                                metadata['theme'] = utils.extract_keywords(meta.get('keywords', None)[0])
-                        
-                        resource_list = []
-
-                        aux = dict()
-
-                        if meta_json.get('files', None) is not None:
-                            url = meta_json.get('files', None)[0]
-                            aux['downloadUrl'] = url.get('links', None).get('self', None)
-                            aux['mediaType'] = url.get('type', None)
-
-                        resource_list.append(aux)
-
-                        metadata['resources'] = resource_list
-                        metadata['modified'] = meta.get('publication_date', None)
-                        #metadata['license'] = requests.get('https://zenodo.org/api/licenses/')
-                        metadata['license'] = None
-                        metadata['source'] = self.domain
-                        
-                        return metadata
-                    else:
-                        # No es tipo csv, zip o excel
-                        print('Buscando siguiente id...')
-                        cont = 1
-                        next_id = ''
-                        for x in craw.get_package_list():
-                            if id == craw.get_package_list()[cont-1]:
-                                next_id = craw.get_package_list()[cont]
-                                print('Siguiente id encontrada: ' + str(next_id))
-                            cont = cont + 1
-                        return(self.get_package(next_id))
-                else:
-                    return('files es none')
+                return metadata
             else:
                 # Timer stops when it can't make any more calls to the API
                 rest = utils.timer_stop()
@@ -106,8 +97,3 @@ class ZenodoCrawler(interface):
             print(traceback.format_exc())
             logger.info(e)
             return None
-        
-craw = ZenodoCrawler('https://zenodo.org')
-
-for id in craw.get_package_list():
-    print(craw.get_package(id))
